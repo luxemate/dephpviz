1c5ad6e3f5fa619cc25a27e7fdc0feba7e209696
luxemate <luxemate1@gmail.com>
2025-02-26 14:33:44 +0200

    Fix webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,12 +8,12 @@ Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
     .addEntry('app', './assets/app.js')
-    .splitEntryChunks()
-    .enableSingleRuntimeChunk()
+    //.splitEntryChunks()
+    .disableSingleRuntimeChunk()
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    .enableVersioning(Encore.isProduction())
+    //.enableVersioning(Encore.isProduction())
     .configureBabelPresetEnv((config) => {
         config.useBuiltIns = 'usage';
         config.corejs = 3;
